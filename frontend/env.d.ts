6b55a2c002ac6d46dd1fadf7f713b693169dc530
Viringe <sora30200@gmail.com>
2022-10-19 18:37:00 +0200

    CERY-14 mise en place du début du frontend

---
+++ frontend/env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+
+
+// http://coverartarchive.org/
+
+DEMO = "https://coverartarchive.org/release/76df3287-6cda-33eb-8e9a-044b5e15ffdd"
---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import carousel from "./components/Carousel.vue";
+
+</script>
+
+<template>
+  <div id="app">
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    <carousel class="menu"/>
+    
+	</div>
+</template>
+
+
+
+<style>
+
+  .menu{
+    margin-top: 3.5rem;
+ 
+  }
+</style>
