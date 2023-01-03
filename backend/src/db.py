80687db20e3f59116c19d263860d9d91232bb51f
Jeremie Opigez <jeremie.opigez@gmail.com>
2023-01-03 18:16:20 +0100

    [deploy_perso]

--- backend/src/db.py
+++ backend/src/db.py
@@ -12,7 +12,7 @@ unix_socket_path = '/cloudsql/ceri-m1-ecommerce-2022:europe-west1:mysql-primary'
 
 
 DATABASE_URL = f"mysql+mysqldb://{mysql_user}:{mysql_password}@/{mysql_table}?unix_socket={unix_socket_path}"
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+# mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
 
 
 # connect_unix_socket initializes a Unix socket connection pool for
@@ -44,11 +44,22 @@ def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
     )
     return pool
 
+
+DATABASE_URL = sqlalchemy.engine.url.URL.create(
+    drivername="mysql+mysqldb",
+    username=mysql_user,
+    password=mysql_password,
+    database=mysql_table,
+    query={"unix_socket": unix_socket_path},
+)
+
 engine = create_engine(DATABASE_URL)
 # engine = connect_unix_socket()
 
+
 def init_db():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
